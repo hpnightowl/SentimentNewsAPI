df3b46aeaca2814eb4b979fd03d350d8154ce576
Harsh <iamhp2k@gmail.com>
2022-12-23 11:13:07 +0000

    fetching news in json from the newsapi.org API

--- main.py
+++ main.py
@@ -1,4 +1,23 @@
 import os
-
+import requests
 # env
-my_secret = os.environ['API_KEY']
+api_key = os.environ['API_KEY']
+
+# endpoint 
+api_url = 'https://newsapi.org/v2/top-headlines'
+
+print("Enter the topic that you want to search for ")
+q = input()
+
+params = {
+  "apiKey":api_key,
+  "q": q,
+  "pageSize" : 100,
+  "page": 1,
+  "language": "en"
+}
+response = requests.get(api_url,params=params)
+
+if response.status_code == 200:
+  print(response.json())
+  
